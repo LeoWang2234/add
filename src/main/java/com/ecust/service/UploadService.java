62ca26e4f2b68dfe431d7d4bd7bc5df228b8e1d4
wangcheng <wangcheng2234@163.com>
2018-04-11 16:35:55 +0800

    增加了文件上传

---
+++ src/main/java/com/ecust/service/UploadService.java
@@ -0,0 +1,10 @@
+package com.ecust.service;
+
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Created by cheng on 2018/4/10.
+ */
+public interface UploadService {
+     boolean praseExcel(String name, MultipartFile file);
+}
